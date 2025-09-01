c89a95bbbc881caa6ffdcfcca7a5690bd44e8aa8
Luiz Paulo Prado <lp1717@gmail.com>
2025-09-01 15:41:08 -0300

    Update playground-string.js

--- playground-string.js
+++ playground-string.js
@@ -11,3 +11,46 @@ var reverseString = function(s) {
         i++;
     }
 };
+
+
+// time: O(n^2)
+// space: O(1)
+var firstUniqChar = function(s) {
+    for(let i=0; i < s.length; i++) { 
+        let count = 0;
+
+        for(let j=0; j < s.length; j++) { 
+            if (s[j] === s[i] && j !== i) {
+                 count += 1;
+            }
+        }
+        
+        if (count === 0) {
+            return i;
+        }
+        
+        count = 0;
+    }
+    
+    return -1;
+};
+
+// time: O(n)
+// space: O(n)
+var firstUniqChar = function(s) {
+    const letters = new Map();
+    
+    for(let i=0; i < s.length; i++) {
+        const letter = s[i];
+        letters.set(letter, (letters.get(letter) || 0) + 1);
+    }
+    
+    for(let i=0; i < s.length; i++) {
+        const letter = s[i];
+        if (letters.has(letter) && letters.get(letter) === 1) {
+            return i;
+        }
+    }
+    
+    return -1;
+};
