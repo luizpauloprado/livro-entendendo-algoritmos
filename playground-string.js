b30587ca648741e1ddcac8487d59e71dcfbcf698
Luiz Paulo Prado <lp1717@gmail.com>
2025-09-02 16:23:34 -0300

    Update playground-string.js

--- playground-string.js
+++ playground-string.js
@@ -54,3 +54,82 @@ var firstUniqChar = function(s) {
     
     return -1;
 };
+
+// String to integer - ATOI
+// time: O(n)
+// space: O(n) - O(k)
+var myAtoi = function(s) {
+    if (!s) return 0;
+    
+    const chars = [...s];
+    
+    const validChars = new Map();
+    validChars.set("0", 0);
+    validChars.set("1", 1);
+    validChars.set("2", 2);
+    validChars.set("3", 3);
+    validChars.set("4", 4);
+    validChars.set("5", 5);
+    validChars.set("6", 6);
+    validChars.set("7", 7);
+    validChars.set("8", 8);
+    validChars.set("9", 9);
+    
+    const validSigns = new Map();
+    validSigns.set("+", "+");
+    validSigns.set("-", "-");
+    
+    if (validChars.has(chars[0]) === false 
+        && validSigns.has(chars[0]) === false 
+        && chars[0] !== " ") return 0;
+    
+    const result = [];
+    
+    for(let i=0;i < chars.length; i++) {
+        const previous = chars[i-1];
+        const current = chars[i];
+        const next = chars[i+1];
+        
+        if (validChars.has(previous) && validChars.has(current) === false) {
+            break;
+        }
+        
+        if (validSigns.has(previous) && validSigns.has(current)) {
+            break;
+        }
+        
+        if (validSigns.has(current) && next === " ") {
+            break;
+        }
+        
+        if (validSigns.has(current) && validChars.has(next) === false) {
+            break;
+        }
+        
+        if (validChars.has(current) === false && validSigns.has(current) === false && current !== " ") {
+            break;
+        }
+        
+        
+        if (validSigns.has(current) && validChars.has(next)) {
+            result.push(current);
+            continue;
+        }
+        
+        if (validChars.has(current)) {
+            result.push(current);
+            continue;
+        }
+    }
+
+    if (result.length === 0) return 0;
+    
+    const min = -Math.pow(2, 31);
+    const max = Math.pow(2, 31) - 1;
+    const value = parseInt(result.join(""), 10);
+    
+    if (value < min) return min; 
+    if (value > max) return max;
+    
+    return value;
+};
